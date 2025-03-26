8424d6529e4bf81c330985ac75818213080d20e1
stinalina <38920418+stinalina@users.noreply.github.com>
2025-03-26 10:46:09 +0100

    adding output param

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,7 @@
 #!/bin/sh -l
 # $@ is a special variable that contains all the parameters that were passed to the script
 echo "Hello $@!"
+echo "answer=42" >> $GITHUB_OUTPUT
+
+echo "### Hello $@! :rocket:" >> $GITHUB_STEP_SUMMARY
+echo "<h3> The answer from Deep Thought is 42 :robot:</h3>" >> $GITHUB_STEP_SUMMARY
