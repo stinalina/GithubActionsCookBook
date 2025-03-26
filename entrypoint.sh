1c3e7fd08b71ee2e9ad55863d000edb2b230c9a6
stinalina <38920418+stinalina@users.noreply.github.com>
2025-03-26 11:13:35 +0100

    try to fix input

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -l
 # $@ is a special variable that contains all the parameters that were passed to the script
-echo "Hello $@!"
+echo "Hello $@!" >> $GITHUB_INPUT
 echo "answer=42" >> $GITHUB_OUTPUT
 
 echo "### Hello $@! :rocket:" >> $GITHUB_STEP_SUMMARY
